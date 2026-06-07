7c643eddf6569e902cf0e57b3072ec1849fcae84
Danya Galkin <danya.galkin2@gmail.com>
2026-06-07 18:14:19 +0300

    st1

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,39 +7,39 @@ bool checkPrime(uint64_t value) {
   if (value < 2) return false;
   if (value == 2) return true;
   if (value % 2 == 0) return false;
-  for (uint64_t i = 3; i <= value / i; i += 2) {
-    if (value % i == 0) return false;
+  for (uint64_t d = 3; d * d <= value; d += 2) {
+    if (value % d == 0) return false;
   }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
   if (n == 0) return 0;
-  uint64_t count = 0;
-  uint64_t value = 1;
-  while (count < n) {
-    value++;
-    if (checkPrime(value)) {
-      count++;
+  uint64_t found = 0;
+  uint64_t num = 2;
+  while (true) {
+    if (checkPrime(num)) {
+      found++;
+      if (found == n) return num;
     }
+    num++;
   }
-  return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t candidate = value + 1;
-  while (!checkPrime(candidate)) {
-    candidate++;
+  uint64_t next = value + 1;
+  while (!checkPrime(next)) {
+    next++;
   }
-  return candidate;
+  return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-  for (uint64_t i = 2; i < hbound; i++) {
-    if (checkPrime(i)) {
-      sum += i;
+  uint64_t total = 0;
+  for (uint64_t num = 2; num < hbound; num++) {
+    if (checkPrime(num)) {
+      total += num;
     }
   }
-  return sum;
+  return total;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -16,8 +16,16 @@ TEST(primes, isPrime_smallest) {
   EXPECT_TRUE(checkPrime(2));
 }
 
+TEST(primes, isPrime_three) {
+  EXPECT_TRUE(checkPrime(3));
+}
+
 TEST(primes, isPrime_four) {
   EXPECT_FALSE(checkPrime(4));
+}
+
+TEST(primes, isPrime_twentyfive) {
+  EXPECT_FALSE(checkPrime(25));
 }
 
 TEST(primes, isPrime_large_prime) {
@@ -28,34 +36,82 @@ TEST(primes, isPrime_large_composite) {
   EXPECT_FALSE(checkPrime(1000));
 }
 
+TEST(primes, isPrime_thousandth) {
+  EXPECT_TRUE(checkPrime(7919));
+}
+
+TEST(primes, isPrime_89squared) {
+  EXPECT_FALSE(checkPrime(7921));
+}
+
+TEST(primes, nth_prime_first) {
+  EXPECT_EQ(2u, nPrime(1));
+}
+
 TEST(primes, nth_prime_third) {
   EXPECT_EQ(5u, nPrime(3));
+}
+
+TEST(primes, nth_prime_sixth) {
+  EXPECT_EQ(13u, nPrime(6));
 }
 
 TEST(primes, nth_prime_tenth) {
   EXPECT_EQ(29u, nPrime(10));
 }
 
+TEST(primes, nth_prime_25th) {
+  EXPECT_EQ(97u, nPrime(25));
+}
+
 TEST(primes, next_after_two) {
   EXPECT_EQ(3u, nextPrime(2));
+}
+
+TEST(primes, next_after_three) {
+  EXPECT_EQ(5u, nextPrime(3));
 }
 
 TEST(primes, next_after_seven) {
   EXPECT_EQ(11u, nextPrime(7));
 }
 
+TEST(primes, next_after_13) {
+  EXPECT_EQ(17u, nextPrime(13));
+}
+
+TEST(primes, next_after_97) {
+  EXPECT_EQ(101u, nextPrime(97));
+}
+
 TEST(primes, next_after_hundred) {
   EXPECT_EQ(101u, nextPrime(100));
+}
+
+TEST(primes, sum_below_2) {
+  EXPECT_EQ(0u, sumPrime(1));
+}
+
+TEST(primes, sum_below_3) {
+  EXPECT_EQ(2u, sumPrime(3));
+}
+
+TEST(primes, sum_to_5) {
+  EXPECT_EQ(5u, sumPrime(5));
+}
+
+TEST(primes, sum_to_12) {
+  EXPECT_EQ(28u, sumPrime(12));
 }
 
 TEST(primes, sum_to_30) {
   EXPECT_EQ(129u, sumPrime(30));
 }
 
-TEST(primes, sum_to_100) {
-  EXPECT_EQ(1060u, sumPrime(100));
+TEST(primes, sum_to_50) {
+  EXPECT_EQ(328u, sumPrime(50));
 }
 
-TEST(primes, sum_below_3) {
-  EXPECT_EQ(2u, sumPrime(3));
+TEST(primes, sum_to_100) {
+  EXPECT_EQ(1060u, sumPrime(100));
 }
