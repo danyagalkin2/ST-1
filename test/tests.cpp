a627558e963aaf564299a5127dfecb9035926f0d
Danya Galkin <danya.galkin2@gmail.com>
2026-06-08 14:16:59 +0300

    update tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,114 +4,89 @@
 #include <cstdint>
 #include "alg.h"
 
-TEST(primes, isPrime_zero) {
-  EXPECT_FALSE(checkPrime(0));
+TEST(CheckPrimeTest, BoundaryValues_0and1and2) {
+    EXPECT_FALSE(checkPrime(0));
+    EXPECT_FALSE(checkPrime(1));
+    EXPECT_TRUE(checkPrime(2));
 }
 
-TEST(primes, isPrime_one) {
-  EXPECT_FALSE(checkPrime(1));
+TEST(CheckPrimeTest, PrimeNumbers_Small_11and13and17) {
+    EXPECT_TRUE(checkPrime(11));
+    EXPECT_TRUE(checkPrime(13));
+    EXPECT_TRUE(checkPrime(17));
 }
 
-TEST(primes, isPrime_smallest) {
-  EXPECT_TRUE(checkPrime(2));
+TEST(CheckPrimeTest, CompositeNumbers_Squares_35and49) {
+    EXPECT_FALSE(checkPrime(35));
+    EXPECT_FALSE(checkPrime(49));
 }
 
-TEST(primes, isPrime_three) {
-  EXPECT_TRUE(checkPrime(3));
+TEST(CheckPrimeTest, PrimeNumbers_Large_103and107) {
+    EXPECT_TRUE(checkPrime(103));
+    EXPECT_TRUE(checkPrime(107));
 }
 
-TEST(primes, isPrime_four) {
-  EXPECT_FALSE(checkPrime(4));
+TEST(CheckPrimeTest, CompositeNumbers_Large_111and121) {
+    EXPECT_FALSE(checkPrime(111));
+    EXPECT_FALSE(checkPrime(121));
 }
 
-TEST(primes, isPrime_twentyfive) {
-  EXPECT_FALSE(checkPrime(25));
+TEST(NPrimeTest, ZeroIndex_Returns0) {
+    EXPECT_EQ(nPrime(0), 0);
 }
 
-TEST(primes, isPrime_large_prime) {
-  EXPECT_TRUE(checkPrime(1009));
+TEST(NPrimeTest, FirstPrime_Returns2) {
+    EXPECT_EQ(nPrime(1), 2);
 }
 
-TEST(primes, isPrime_large_composite) {
-  EXPECT_FALSE(checkPrime(1000));
+TEST(NPrimeTest, SmallIndices_4and5and7) {
+    EXPECT_EQ(nPrime(4), 7);
+    EXPECT_EQ(nPrime(5), 11);
+    EXPECT_EQ(nPrime(7), 17);
 }
 
-TEST(primes, isPrime_thousandth) {
-  EXPECT_TRUE(checkPrime(7919));
+TEST(NPrimeTest, LargerIndices_12and18) {
+    EXPECT_EQ(nPrime(12), 37);
+    EXPECT_EQ(nPrime(18), 61);
 }
 
-TEST(primes, isPrime_89squared) {
-  EXPECT_FALSE(checkPrime(7921));
+TEST(NextPrimeTest, AfterSmallComposite_4and6) {
+    EXPECT_EQ(nextPrime(4), 5);
+    EXPECT_EQ(nextPrime(6), 7);
 }
 
-TEST(primes, nth_prime_first) {
-  EXPECT_EQ(2u, nPrime(1));
+TEST(NextPrimeTest, AfterPrime_23and29and40) {
+    EXPECT_EQ(nextPrime(23), 29);
+    EXPECT_EQ(nextPrime(29), 31);
+    EXPECT_EQ(nextPrime(40), 41);
 }
 
-TEST(primes, nth_prime_third) {
-  EXPECT_EQ(5u, nPrime(3));
+TEST(NextPrimeTest, AfterLargerComposite_48and56) {
+    EXPECT_EQ(nextPrime(48), 53);
+    EXPECT_EQ(nextPrime(56), 59);
 }
 
-TEST(primes, nth_prime_sixth) {
-  EXPECT_EQ(13u, nPrime(6));
+TEST(SumPrimeTest, BoundaryValues_0and1and2) {
+    EXPECT_EQ(sumPrime(0), 0);
+    EXPECT_EQ(sumPrime(1), 0);
+    EXPECT_EQ(sumPrime(2), 0);
 }
 
-TEST(primes, nth_prime_tenth) {
-  EXPECT_EQ(29u, nPrime(10));
+TEST(SumPrimeTest, SmallBounds_7and10and18) {
+    EXPECT_EQ(sumPrime(7), 10);
+    EXPECT_EQ(sumPrime(10), 17);
+    EXPECT_EQ(sumPrime(18), 58);
 }
 
-TEST(primes, nth_prime_25th) {
-  EXPECT_EQ(97u, nPrime(25));
+TEST(SumPrimeTest, MediumBounds_25and35) {
+    EXPECT_EQ(sumPrime(25), 100);
+    EXPECT_EQ(sumPrime(35), 160);
 }
 
-TEST(primes, next_after_two) {
-  EXPECT_EQ(3u, nextPrime(2));
+TEST(SumPrimeTest, LargeBound_55) {
+    EXPECT_EQ(sumPrime(55), 381);
 }
 
-TEST(primes, next_after_three) {
-  EXPECT_EQ(5u, nextPrime(3));
-}
-
-TEST(primes, next_after_seven) {
-  EXPECT_EQ(11u, nextPrime(7));
-}
-
-TEST(primes, next_after_13) {
-  EXPECT_EQ(17u, nextPrime(13));
-}
-
-TEST(primes, next_after_97) {
-  EXPECT_EQ(101u, nextPrime(97));
-}
-
-TEST(primes, next_after_hundred) {
-  EXPECT_EQ(101u, nextPrime(100));
-}
-
-TEST(primes, sum_below_2) {
-  EXPECT_EQ(0u, sumPrime(1));
-}
-
-TEST(primes, sum_below_3) {
-  EXPECT_EQ(2u, sumPrime(3));
-}
-
-TEST(primes, sum_to_5) {
-  EXPECT_EQ(5u, sumPrime(5));
-}
-
-TEST(primes, sum_to_12) {
-  EXPECT_EQ(28u, sumPrime(12));
-}
-
-TEST(primes, sum_to_30) {
-  EXPECT_EQ(129u, sumPrime(30));
-}
-
-TEST(primes, sum_to_50) {
-  EXPECT_EQ(328u, sumPrime(50));
-}
-
-TEST(primes, sum_to_100) {
-  EXPECT_EQ(1060u, sumPrime(100));
+TEST(SumPrimeTest, VeryLargeBound_100) {
+    EXPECT_EQ(sumPrime(100), 1060);
 }
